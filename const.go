69697142f3315388e59112e810d137d8ea11f694
arata234 <aratatakamatsu@gmail.com>
2023-04-12 23:06:43 +0900

    Create const.go

---
+++ const.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const (
+	Pi       = 3.14
+	URL      = "https://google.com/"
+	SiteName = "google"
+)
+
+func main() {
+	fmt.Println(Pi)
+	fmt.Println(URL)
+	fmt.Println(SiteName)
+}
