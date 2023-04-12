25de84a1ed4477f12decb40baf3eabf970ae4206
arata234 <aratatakamatsu@gmail.com>
2023-04-12 23:06:46 +0900

    Create operator.go

---
+++ operator.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//算術演算子
+	fmt.Println(1 + 2)
+	fmt.Println("ABC" + "DE")
+
+	fmt.Println(2 - 1)
+	fmt.Println(3 * 2)
+	fmt.Println(80 / 5)
+	fmt.Println(9 % 4)
+
+	n := 0
+	n += 4
+	fmt.Println(n)
+	n++
+	fmt.Println(n)
+	n--
+	fmt.Println(n)
+}
